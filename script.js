7744ce46b00c84f8222928cd78101ecca5c94161
Teff <101304001+tefftorm@users.noreply.github.com>
2025-04-10 21:58:09 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const card = document.getElementById("card");
 
-  card.addEventListener("click", function () {
-      card.classList.toggle("flipped");
-  });
-});
