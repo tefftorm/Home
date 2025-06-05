4561922b85743095d5c0f639171d4f951d30c470
Teff <101304001+tefftorm@users.noreply.github.com>
2025-06-04 22:04:01 -0300

    Update contador.php

--- contador.php
+++ contador.php
@@ -1,13 +1,1 @@
-<?php
-$archivo = "contador.txt";
 
-if (!file_exists($archivo)) {
-    file_put_contents($archivo, "0");
-}
-
-$visitas = (int) file_get_contents($archivo);
-$visitas++;
-file_put_contents($archivo, $visitas);
-
-echo $visitas;
-?>
